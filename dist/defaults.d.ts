7aed53c2d954d7c165db69f5c377514eba443613
whyamiroot <3541904+whyamiroot@users.noreply.github.com>
2024-08-28 15:31:57 +0200

    remove dist

--- dist/defaults.d.ts
+++
@@ -1,7 +0,0 @@
-import type { CustomBackendConfig, HeaderDict, NotarizationOptions } from './types';
-export declare const DEFAULT_TIMEOUT_MS = 5000;
-export declare const DEFAULT_NOTARIZATION_HEADERS: HeaderDict;
-export declare const DEFAULT_FETCH_OPTIONS: Pick<RequestInit, 'cache' | 'keepalive' | 'mode' | 'referrer' | 'redirect'>;
-export declare const DEFAULT_NOTARIZATION_OPTIONS: NotarizationOptions;
-export declare const DEFAULT_NOTARIZATION_BACKENDS: CustomBackendConfig[];
-export declare const DEFAULT_VERIFICATION_BACKEND: CustomBackendConfig;
